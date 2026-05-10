74bfb1aa855d7ac81ce10e09dc6cd6d7c2ef11f9
Elias <eliasmamani0403@gmail.com>
2026-05-10 02:33:21 -0300

    Agrego Header, Nav, Footer y estilos del TP1

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+const Nav = () => {
+    return (
+        <nav className="main-nav">
+            <ul>
+                <li><a href="/">Dashboard</a></li>
+                <li><a href="/">Proyectos</a></li>
+                <li><a href="/">Perfil</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
