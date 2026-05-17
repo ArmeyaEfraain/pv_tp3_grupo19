7492b47d4eb6821b4d5e13c7ab87c9c1ca7a6cc8
Armeya Efraain <armeyaefrain@gmail.com>
2026-05-17 18:16:41 -0300

    declaracion de useState e implementacion de useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import { useState, useEffect } from 'react';
+
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import ListaProyectos from './components/ListaProyectos';
-import { proyectoService } from './services/proyectoService';
+
+import proyectoService from './services/proyectoService';
+
 import './css/styles.css';
 
 function App() {
+
+  const [proyectos, setProyectos] = useState([]);
+
+  useEffect(() => {
+    const datos = proyectoService.obtenerProyectos();
+    setProyectos(datos);
+  }, []);
+
   return (
     <>
       <Header />
       <Nav />
+
       <main id="center">
-        <ListaProyectos />
+        <ListaProyectos proyectos={proyectos} />
       </main>
+
       <Footer />
     </>
   );
