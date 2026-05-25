033dda97855c9bee2749318820a864767ca5fb36
Elias <eliasmamani0403@gmail.com>
2026-05-25 04:13:12 -0300

    feat: Integracion de RegistroActividad, resolucion de filtro con Estado Derivado y README

--- src/components/ListaProyectos.jsx
+++ src/components/ListaProyectos.jsx
@@ -2,17 +2,28 @@ import React, { useState } from 'react';
 import ProyectoCard from './ProyectoCard';
 import FormularioProyecto from './FormularioProyecto';
 
-const ListaProyectos = ({ proyectos, onEliminar, onAgregar }) => {
+const ListaProyectos = ({ proyectos, onEliminar, onAgregar, onVerDetalle, busqueda, onBusqueda}) => {
   return (
     <main>
       <FormularioProyecto onAgregar={onAgregar} />
+      
+      <div className="buscador-wrapper">
+        <input
+          type="text"
+          placeholder="Buscar proyecto por título..."
+          value={busqueda}
+          onChange={(e) => onBusqueda(e.target.value)}
+          className="buscador-input"
+        />
+      </div>
 
       <div className="lista-container">
         {proyectos.map((proy) => (
           <ProyectoCard 
             key={proy.id} 
             proyecto={proy} 
-            onEliminar={onEliminar} 
+            onEliminar={onEliminar}
+            onVerDetalle={onVerDetalle} 
           />
         ))}
       </div>
